0bba15cd6b0680473e707a4ef5129f5ddf101315
wizard-999 <152674949+wizard-999@users.noreply.github.com>
2024-07-08 15:04:10 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const cors = require('cors');
 
 app.use(cors({
-    origin: 'https://recipe-sharing-hub.vercel.app', // Update with your frontend URL
+    origin: 'https://recipe-sharing-hub-frontend.vercel.app/', // Update with your frontend URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
 }));
